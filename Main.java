692c96237700848bfcf7bd111e790ff9a1634e61
mcmunchie <89753143+mcmunchie@users.noreply.github.com>
2022-05-18 01:41:17 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Maze maze = new Maze();
+        maze.maze();
+    }
+}
